455bc3b043e18bb358e59812fa821da48abc8a56
sandipakolkar906 <mail2sandipakolkar@gmail.com>
2025-03-09 08:44:25 +0530

    Updated Report Format to change it to .json to run from jenkins

--- src/test/java/com/qa/runner/Runner.java
+++ src/test/java/com/qa/runner/Runner.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
-
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepdefinations" }, tags={"@DryRun1"},plugin = { "pretty",
-		"html:target/cucumber-reports" })
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepdefinations" }, tags = {
+		"@DryRun" }, plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
 
 public class Runner {
+
+	// "html:target/cucumber-reports" }
 
 }
