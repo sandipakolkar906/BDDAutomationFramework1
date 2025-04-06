2f9d7a2ad0eb80781e37c711a702f0ed07727cad
sandipakolkar906 <mail2sandipakolkar@gmail.com>
2025-04-06 10:10:06 +0530

    Added Event Tests

--- src/test/java/com/qa/stepdefinations/ClaimsCRUD.java
+++ src/test/java/com/qa/stepdefinations/ClaimsCRUD.java
@@ -97,6 +97,45 @@ public class ClaimsCRUD extends Base {
 		Assert.assertEquals("50.00", objClaimsPage.getTotalExpenseAmount(scenario));
 	}
 
+	// Event CRUD Steps
+
+	@When("^I navigate to Configuration menu and Select Event option$")
+	public void i_navigate_to_Configuration_menu_and_Select_Event_option() throws Throwable {
+		scenario.write("I navigate to Configuration menu and Select Event option");
+		objClaimsPage.navigateToConfigurationMenu(scenario);
+		objClaimsPage.navigateToEvents(scenario);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+	}
+
+	@When("^I add new Event with below event Name$")
+	public void i_add_new_Event_with_below_event_Name(DataTable eventNameDataTable) throws Throwable {
+		scenario.write("I add new Event with below event Name");
+		objClaimsPage.addEvent(scenario, eventNameDataTable.raw().get(0).get(1));
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+	}
+
+	@Then("^I Search Event with below eventName$")
+	public void i_Search_Event_with_below_eventName(DataTable eventNameDataTable) throws Throwable {
+		scenario.write("I Search Event with below eventName");
+		Assert.assertEquals(eventNameDataTable.raw().get(0).get(1),
+				objClaimsPage.searchEvent(scenario, eventNameDataTable.raw().get(0).get(1)));
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+	}
+
+	@When("^I edit the Event and change name$")
+	public void i_edit_the_Event_and_change_name(DataTable eventNameDataTable) throws Throwable {
+		scenario.write("I edit the Event and change name");
+		objClaimsPage.editEvent(scenario, eventNameDataTable.raw().get(0).get(1));
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+	}
+
+	@Then("^I delete the newly added and updated Event$")
+	public void i_delete_the_newly_added_and_updated_Event() throws Throwable {
+		scenario.write("I delete the newly added and updated Event");
+		objClaimsPage.deleteEvent(scenario);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+	}
+
 	@After
 	public void closeApplication(Scenario scenario) {
 
